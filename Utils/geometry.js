faaf42e99b8ebab1fd9d5f443a6d4eefdb2b329a
kalev mölder <artkalev@gmail.com>
2017-12-08 00:11:53 +0200

    Changes to be committed: 	modified:   Utils/geometry.js

--- Utils/geometry.js
+++ Utils/geometry.js
@@ -6,7 +6,7 @@ function ControlPoint( position, handleIn, handleOut ){
 	this.position = position || new Vec2(0,0);
 	this.handleIn = handleIn || new Vec2(-1,0);
 	this.handleOut = handleOut || new Vec2(1,0);
-}
+};
 
 function BezierCurve2D(){
 	this.controlPoints = [ 
@@ -15,7 +15,23 @@ function BezierCurve2D(){
 	];
 	this.bakeDensity = 64;
 	this.bakedPoints = [];
-}
+};
 BezierCurve2D.prototype.Bake = function(){
 	// todo
+	// calculate and store interpolated points
+	// between control points
+};
+
+function BezierPath2D(){
+	this.controlPoints = [
+		new Vec2(-1,0), new Vec2( -1, 1),
+		new Vec2( 1,0), new Vec2(  1,-1)
+	];
+	this.bakeDensity = 64;
+	this.bakedPoints = [];
+};
+BezierCurve2D.prototype.Bake = function(){
+	// todo
+	// calculate and store interpolated points
+	// between control points
 };
